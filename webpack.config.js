af2e6a2067f6cd24159b82fa9a6eead553c4a00e
Adam Cross <adamfgcross@gmail.com>
2023-06-06 15:22:40 -0400

    package

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
 const path = require("path");
-const webpack = require("webpack");
 
 module.exports = {
-    entry: "./src/cross-js-base.js",
-    output: {
-        path: path.resolve(__dirname, "./"),
-        filename: "cross-js-base.js",
-        library: "crossJsBase",
-        libraryTarget: "umd",
-        umdNamedDefine: true
+    entry : "./src/cross-js-base.js",
+    mode : "production",
+    output : {
+        path : path.resolve(__dirname, "./"),
+        filename : "cross-js-base.js",
+        library : "crossJsBase",
+        libraryTarget : "umd",
+        umdNamedDefine : true
     },
-    module: {
-        rules: [
+    module : {
+        rules : [
             {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              use: 'babel-loader',
-            },
-          ]
+                test : /\.js$/,
+                exclude : /node_modules/,
+                use : "babel-loader"
+            }
+        ]
     },
-    optimization: {
-        minimize: true
+    optimization : {
+        minimize : true
     }
 };
